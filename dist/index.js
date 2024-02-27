317d3f3c096b53452e2bd9441630a3545ab0b550
Manuel <cm039398@gmail.com>
2024-02-26 19:24:06 -0600

    Tarea Terminada

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,19 @@
 "use strict";
-// import { Planeta } from "./planeta";
-// const listaPlanetas: Planeta[] = [
-//     new Planeta('Marte', 3.71),
-//     new Planeta('Jupiter', 24.79),
-//     new Planeta('Luna', 1.63)
-// ];
 var btnCalcularPeso = document.getElementById('btnCalcularPeso');
 if (btnCalcularPeso) {
     btnCalcularPeso.addEventListener('click', function () {
         var nombreInput = document.getElementById('txtNombre');
         var pesoInput = document.getElementById('txtPeso');
-        if (nombreInput && pesoInput) {
+        var resultado = document.getElementById('resultado');
+        if (nombreInput && pesoInput && resultado) {
             var nombre = nombreInput.value;
             var peso = parseFloat(pesoInput.value);
             if (peso) {
                 console.log(nombre);
                 console.log(peso);
-                // listaPlanetas.forEach(planeta => {
                 var gravedad = 3.71;
                 var pesoEnPlaneta = peso * gravedad;
-                console.log("".concat(nombre, ", tu peso en Marte es de ").concat(pesoEnPlaneta, " lbs"));
-                // });
+                resultado.innerText = "".concat(nombre, ", tu peso en Marte es de ").concat(pesoEnPlaneta, " lbs");
             }
             else {
                 console.log('¡Por favor, introduce un peso válido!');
--- index.ts
+++ index.ts
@@ -1,32 +1,23 @@
-// import { Planeta } from "./planeta";
-
-// const listaPlanetas: Planeta[] = [
-//     new Planeta('Marte', 3.71),
-//     new Planeta('Jupiter', 24.79),
-//     new Planeta('Luna', 1.63)
-// ];
 
 let btnCalcularPeso = document.getElementById('btnCalcularPeso');
 if (btnCalcularPeso) {
     btnCalcularPeso.addEventListener('click', () => {
         let nombreInput = document.getElementById('txtNombre') as HTMLInputElement;
         let pesoInput = document.getElementById('txtPeso') as HTMLInputElement;
+        let resultado = document.getElementById('resultado') as HTMLHeadingElement;
 
-        if (nombreInput && pesoInput) {
+        if (nombreInput && pesoInput && resultado) {
             let nombre = nombreInput.value;
             let peso = parseFloat(pesoInput.value);
-
             if (peso) {
                 console.log(nombre);
                 console.log(peso);
 
-                // listaPlanetas.forEach(planeta => {
                     let gravedad: number = 3.71;
                     let pesoEnPlaneta: number = peso * gravedad;
-                    console.log(`${nombre}, tu peso en Marte es de ${pesoEnPlaneta} lbs`);
-                // });
-            } else {
-                console.log('¡Por favor, introduce un peso válido!');
+                    resultado.innerText = `${nombre}, tu peso en Marte es de ${pesoEnPlaneta} lbs`                    
+                } else {
+                    console.log('¡Por favor, introduce un peso válido!');
             }
         } else {
             console.log('¡Elementos de entrada no encontrados!');
